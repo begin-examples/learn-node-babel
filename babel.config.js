e30f290c1dfd65fd19d765c4679aea4cd4faf612
Paul Chin Jr <pchinjr@gmail.com>
2020-03-16 22:25:54 -0400

    initial commit

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+module.exports = function(api) {
+  api.cache(false)
+  return {
+    presets: [[
+      "@babel/preset-env",
+      {targets: {node: true}, modules: 'cjs'}
+    ]],
+    plugins: [[
+      "@babel/plugin-proposal-optional-chaining"
+    ]]
+  }
+}
